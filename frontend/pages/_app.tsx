b6fa55967871c01633cd4a2250fa9d443fb7b23f
vangona <rlarhksrud14@gmail.com>
2022-11-14 18:07:25 +0900

    [env] nextjs 초기 파일 삭제

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,8 @@
-import '../styles/globals.css'
+import '../sass/globals.scss'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
 }
+
+export default App;
