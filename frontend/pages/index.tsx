6534e0bc8cd02141eb7aa7e7232fe88c7b3e06c4
vangona <rlarhksrud14@gmail.com>
2022-11-17 20:23:20 +0900

    [chore] 데이터 수신 시 alert 문구 수정

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -14,7 +14,7 @@ const Home = () => {
     axios
       .get('http://waglewagle.link/demo')
       .then((response) => {
-        alert(response.data.TestKey);
+        alert(`서버에서 값을 잘 받아왔습니다. 값 : ${response.data.TestKey}`);
       })
       .catch(() => {
         alert('서버 통신에 실패했습니다.');
