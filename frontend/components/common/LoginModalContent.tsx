41d3760edc4ce7fe6e648525053539c2d7034770
vangona <rlarhksrud14@gmail.com>
2022-11-23 00:49:09 +0900

    [feat] 로그인 버튼 api 연결

--- frontend/components/common/LoginModalContent.tsx
+++ frontend/components/common/LoginModalContent.tsx
@@ -1,14 +1,40 @@
 import classnames from 'classnames/bind';
 import styles from '@sass/components/common/LoginModalContent.module.scss';
 import { AiFillGithub } from 'react-icons/ai';
+import { ChangeEvent, useState } from 'react';
+import apis from '../../apis/apis';
 const cx = classnames.bind(styles);
 
 const LoginModalContent = () => {
+  const [username, setUsername] = useState<string>('');
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
+
+  const handleSubmitUsernameLogin = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    apis.fetchLogin(username);
+    setUsername('');
+  };
+
   return (
     <div className={cx('container')}>
       <h2 className={cx('title')}>로그인</h2>
-      <input className={cx('username-input')} placeholder='유저이름' />
-      <button className={cx('login-button')}>로그인</button>
+      <form
+        className={cx('username-login-form')}
+        onSubmit={handleSubmitUsernameLogin}
+      >
+        <input
+          value={username}
+          onChange={onChange}
+          className={cx('username-input')}
+          placeholder='유저이름'
+        />
+        <button type='submit' className={cx('login-button')}>
+          로그인
+        </button>
+      </form>
       <hr />
       <button className={cx('github-login-button')}>
         <AiFillGithub />
