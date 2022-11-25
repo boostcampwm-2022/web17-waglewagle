b7f53dca16451d6b8caa7d3207cbdfcc688aa2d0
vangona <rlarhksrud14@gmail.com>
2022-11-25 10:11:40 +0900

    [fix] 제니퍼 프론트엔드 빠져있어서 추가

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,25 @@
 import '@sass/globals.scss';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        async
+        src='https://d-collect.jennifersoft.com/c0bb5fef/demian.js'
+      />
+      <Script id='jennifer-frontend'>
+        {`
+    (function(j,ennifer) {
+      j['dmndata']=[];j['jenniferFront']=function(args){window.dmndata.push(args)};
+      j['dmnaid']=ennifer;j['dmnatime']=new Date();j['dmnanocookie']=false;j['dmnajennifer']='JENNIFER_FRONT@INTG';
+  }(window, 'c0bb5fef'));
+        `}
+      </Script>
+      <Component {...pageProps} />;
+    </>
+  );
 };
 
 export default App;
