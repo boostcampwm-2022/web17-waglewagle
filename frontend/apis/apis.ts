e69f87565540e968244ad10c235ffd3834f509e7
vangona <rlarhksrud14@gmail.com>
2022-11-29 14:14:59 +0900

    [feat] axios api 수신 확인
    Co-authored-by: 문성현 <shine7329@gmail.com>

--- frontend/apis/apis.ts
+++ frontend/apis/apis.ts
@@ -1,8 +1,9 @@
 // 일단 스타일 신경 안쓰고 만들어두었읍니다.
 import axios from 'axios';
+import config from '../config';
 
 const instance = axios.create({
-  baseURL: 'http://www.waglewagle.link/api',
+  baseURL: `${config.API_HOST}`,
 });
 
 // TODO : apis 스타일 맞추기
@@ -13,8 +14,15 @@ const fetchLogin = async (username: string) => {
   return response;
 };
 
+const getKeywords = async () => {
+  const response = await instance.get('/v1/keyword/1');
+
+  return response;
+};
+
 const apis = {
   fetchLogin,
+  getKeywords,
 };
 
 export default apis;
---
+++ frontend/config.ts
@@ -0,0 +1,7 @@
+const API_HOST = process.env.NEXT_PUBLIC_API_HOST;
+
+const config = {
+  API_HOST,
+};
+
+export default config;
