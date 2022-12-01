7a07d91a0fe1d369448351bdb48274f5b7329c19
Seungmin <haradwaith@naver.com>
2022-12-02 02:30:02 +0900

    [feat] 커뮤니티 가입 기능 구현

---
+++ rest/src/main/java/com/waglewagle/rest/communityUser/CommunityUserService.java
@@ -0,0 +1,34 @@
+package com.waglewagle.rest.communityUser;
+
+import com.waglewagle.rest.community.CommunityRepository;
+import com.waglewagle.rest.community.Community;
+import com.waglewagle.rest.user.User;
+import com.waglewagle.rest.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CommunityUserService {
+
+    private final CommunityUserRepository communityUserRepository;
+    private final CommunityRepository communityRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public boolean joinCommunity(Long userId, Long communityId) {
+        if (communityUserRepository.findByUserIdCommunityId(userId, communityId) != null) {
+            System.out.println("Validated");
+            return false;
+        }
+
+        User user = userRepository.findById(userId);
+        Community community = communityRepository.findOneById(communityId);
+        CommunityUser communityUser = new CommunityUser(user, community);
+
+        communityUserRepository.save(communityUser);
+        return true;
+
+    }
+}
