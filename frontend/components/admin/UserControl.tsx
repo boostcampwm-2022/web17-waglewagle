25f866060430b148f44104086697a3210ef1e8b7
문성현 <shine7329@gmail.com>
2022-12-07 21:41:12 +0900

    [feat] 관리자 페이지 유저 목록 표시

--- frontend/components/admin/UserControl.tsx
+++ frontend/components/admin/UserControl.tsx
@@ -1,7 +1,19 @@
+import useKeywordUserListQuery from '@hooks/useKeywordUserListQuery';
+import styles from '@sass/components/admin/UserControl.module.scss';
+import classnames from 'classnames/bind';
+const cx = classnames.bind(styles);
+
 const UserControl = () => {
+  const { data: userList } = useKeywordUserListQuery('123');
+
   return (
     <>
-      <h4>유저 관리</h4>
+      <h4>참여 중인 유저</h4>
+      <ul className={cx('user-list')}>
+        {userList?.map((user) => (
+          <li key={user.userId}>{user.username}</li>
+        ))}
+      </ul>
     </>
   );
 };
