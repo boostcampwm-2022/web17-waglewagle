f32d17cf57399cda573c60b27a84f50d5352df72
vangona <rlarhksrud14@gmail.com>
2022-12-08 23:50:03 +0900

    [feat] 변경된 /user/me 적용
    communityId 없이도 응답 수신

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -10,28 +10,24 @@ import HomeDescription from '@components/home/HomeDescription';
 import SeoHead from '@components/common/Head';
 import { useRouter } from 'next/router';
 import config from '../config';
-import useMockUserMe from '@hooks/useMockUserMe';
-import { useEffect, useState } from 'react';
-import apis from '../apis/apis';
-import { MVP_DEFAULT } from '@constants/constants';
+import { useState, useEffect } from 'react';
 import { LoginModalContent, Modal } from '@components/common';
+import useUserMe from '@hooks/useUserMe';
 
 const Home = () => {
-  const [isOpenLoginModal, setIsOpenLoginModal] = useState<boolean>(false);
   const router = useRouter();
-  // TODO: 나중에 /user/me 수정되면 삭제하기
-  const mockUserData = useMockUserMe();
+  const [isOpenLoginModal, setIsOpenLoginModal] = useState<boolean>(false);
+  const userData = useUserMe();
 
   const openLoginModal = () => {
     setIsOpenLoginModal(true);
   };
 
   useEffect(() => {
-    if (mockUserData) {
-      apis.joinCommunity(MVP_DEFAULT.COMMUNITY_ID);
+    if (userData) {
       router.push('/main');
     }
-  }, [mockUserData]);
+  }, [userData]);
 
   return (
     <HomeLayout>
