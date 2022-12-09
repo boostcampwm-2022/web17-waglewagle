05d4c9dfad0911e7a40351fbd8d977b789a07bba
문성현 <shine7329@gmail.com>
2022-12-09 14:23:41 +0900

    [refactor] 키워드 api 모듈 분리

--- frontend/apis/apis.ts
+++ frontend/apis/apis.ts
@@ -1,18 +1,8 @@
-import {
-  AddKeywordData,
-  CommunityData,
-  JoinKeywordData,
-  KeywordData,
-  KeywordRelatedData,
-  KeywordUser,
-  MyKeywordData,
-  ThreadData,
-  UserData,
-} from '#types/types';
+import type { CommunityData, KeywordUser, UserData } from '#types/types';
 import axios from 'axios';
 import config from '../config';
 
-const apiInstance = axios.create({
+export const apiInstance = axios.create({
   baseURL: `${config.API_HOST}`,
 });
 
@@ -22,12 +12,6 @@ const fetchLogin = async (username: string) => {
   });
 
   return response;
-};
-
-const getKeywords = async (id: string): Promise<KeywordData[]> => {
-  const response = await apiInstance.get(`/v1/keyword/${id}`);
-
-  return response.data;
 };
 
 const getUserCommunityList = async (): Promise<CommunityData[]> => {
@@ -57,44 +41,6 @@ const getUserData = async (communityId?: string): Promise<UserData> => {
   return response.data;
 };
 
-const getKeywordAssociations = async (
-  id: string,
-): Promise<KeywordRelatedData[]> => {
-  const response = await apiInstance.get('/v1/keyword/associations', {
-    params: {
-      'keyword-id': id,
-    },
-  });
-
-  return response.data;
-};
-
-const addKeyword = async (
-  addKeywordData: AddKeywordData,
-): Promise<MyKeywordData> => {
-  const response = await apiInstance.post('/v1/keyword', addKeywordData);
-
-  return response.data;
-};
-
-const joinKeyword = async (joinKeywordData: JoinKeywordData) => {
-  await apiInstance.post('/v1/keyword/join', joinKeywordData);
-};
-
-const disjoinKeyword = async (disjoinKeywordData: JoinKeywordData) => {
-  await apiInstance.delete('/v1/keyword/disjoin', {
-    data: disjoinKeywordData,
-  });
-};
-
-const getKeywordThreads = async (keywordId: string): Promise<ThreadData[]> => {
-  const response = await apiInstance.get(
-    `/v1/thread/keyword?keyword-id=${keywordId}`,
-  );
-
-  return response.data;
-};
-
 const getKeywordUsers = async (keywordId: string): Promise<KeywordUser[]> => {
   const response = await apiInstance.get(
     `/v1/user/keyword?keyword-id=${keywordId}`,
@@ -103,89 +49,13 @@ const getKeywordUsers = async (keywordId: string): Promise<KeywordUser[]> => {
   return response.data;
 };
 
-const addThread = async (keywordId: string, content: string) => {
-  const response = await apiInstance.post('/v1/thread', {
-    keywordId,
-    content,
-  });
-
-  return response.data;
-};
-
-const addComments = async (
-  keywordId: string,
-  content: string,
-  parentThreadId: string,
-) => {
-  const response = await apiInstance.post('/v1/thread', {
-    keywordId,
-    content,
-    parentThreadId,
-  });
-
-  return response.data;
-};
-
-const deleteThread = async (threadId: string) => {
-  const response = await apiInstance.delete('/v1/thread', {
-    data: { threadId },
-  });
-
-  return response.data;
-};
-
-const deleteKeyword = async (communityId: string, keywordIdList: string[]) => {
-  const response = await apiInstance.delete('/v1/keyword', {
-    data: {
-      communityId,
-      keywordIdList,
-    },
-  });
-
-  return response.data;
-};
-
-const mergeKeyword = async (
-  communityId: string,
-  destinationKeywordId: string,
-  sourceKeywordIdList: string[],
-) => {
-  const response = await apiInstance.put('/v1/keyword/merge', {
-    communityId,
-    destinationKeywordId,
-    sourceKeywordIdList,
-  });
-
-  return response.data;
-};
-
-const getMyKeywordList = async (
-  communityId: string,
-): Promise<MyKeywordData[]> => {
-  const response = await apiInstance.get(`/v1/keyword/user/${communityId}`);
-
-  return response.data;
-};
-
 const apis = {
   fetchLogin,
-  getKeywords,
-  joinKeyword,
-  disjoinKeyword,
   getUserCommunityList,
   joinCommunity,
   updateFirstVisitInCommunity,
-  addKeyword,
   getUserData,
-  getKeywordAssociations,
-  getKeywordThreads,
   getKeywordUsers,
-  addThread,
-  addComments,
-  deleteThread,
-  deleteKeyword,
-  mergeKeyword,
-  getMyKeywordList,
 };
 
 export default apis;
---
+++ frontend/apis/keyword.ts
@@ -0,0 +1,85 @@
+import type {
+  KeywordData,
+  KeywordRelatedData,
+  MyKeywordData,
+} from '#types/types';
+import { apiInstance } from '@apis/apis';
+import { instance } from '@apis/instance';
+import { AxiosResponse } from 'axios';
+
+const getKeywords = (
+  keywordId: string,
+): Promise<AxiosResponse<KeywordData[]>> =>
+  instance.get(`/v1/keyword/${keywordId}`);
+
+const getKeywordAssociations = (
+  keywordId: string,
+): Promise<AxiosResponse<KeywordRelatedData[]>> =>
+  instance.get('/v1/keyword/associations', {
+    params: {
+      'keyword-id': keywordId,
+    },
+  });
+
+const getMyKeywordList = (
+  communityId: string,
+): Promise<AxiosResponse<MyKeywordData[]>> =>
+  instance.get(`/v1/keyword/user/${communityId}`);
+
+export type AddKeywordRequestBody = {
+  keywordName: string;
+  communityId: string;
+};
+
+const addKeyword = (
+  addKeywordRequestBody: AddKeywordRequestBody,
+): Promise<AxiosResponse<MyKeywordData>> =>
+  apiInstance.post('/v1/keyword', addKeywordRequestBody);
+
+type JoinKeywordRequestBody = {
+  keywordId: string;
+  communityId: string;
+};
+
+const joinKeyword = (joinRequestBody: JoinKeywordRequestBody) =>
+  instance.post('/v1/keyword/join', joinRequestBody);
+
+type DisjoinKeywordRequestBody = {
+  keywordId: string;
+  communityId: string;
+};
+
+const disjoinKeyword = (disjoinKeywordRequestBody: DisjoinKeywordRequestBody) =>
+  instance.delete('/v1/keyword/disjoin', {
+    data: disjoinKeywordRequestBody,
+  });
+
+type DeleteKeywordRequestBody = {
+  communityId: string;
+  keywordIdList: string[];
+};
+
+const deleteKeyword = (deleteKeywordRequestBody: DeleteKeywordRequestBody) =>
+  instance.delete('/v1/keyword', {
+    data: deleteKeywordRequestBody,
+  });
+
+type MergeKeywordRequestBody = {
+  communityId: string;
+  destinationKeywordId: string;
+  sourceKeywordIdList: string[];
+};
+
+const mergeKeyword = (mergeKeywordRequestBody: MergeKeywordRequestBody) =>
+  instance.put('/v1/keyword/merge', mergeKeywordRequestBody);
+
+export const keyword = {
+  getKeywords,
+  getKeywordAssociations,
+  getMyKeywordList,
+  addKeyword,
+  joinKeyword,
+  disjoinKeyword,
+  deleteKeyword,
+  mergeKeyword,
+};
