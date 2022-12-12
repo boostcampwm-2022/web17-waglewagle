54396fe1b63f854ae0920a354c1cf319f4b5ed8c
vangona <rlarhksrud14@gmail.com>
2022-12-12 23:56:35 +0900

    [feat] 커뮤니티 키워드 목록 구현

--- frontend/components/community/CommunityKeywordList.tsx
+++ frontend/components/community/CommunityKeywordList.tsx
@@ -1,6 +1,10 @@
-import { Loading } from '@components/common';
-import { useKeywordListQuery } from '@hooks/keyword';
 import { useRouter } from 'next/router';
+import classnames from 'classnames/bind';
+import styles from '@sass/components/community/CommunityKeywordList.module.scss';
+import { useKeywordListQuery } from '@hooks/keyword';
+import { Loading } from '@components/common';
+
+const cx = classnames.bind(styles);
 
 const CommunityKeywordList = () => {
   const router = useRouter();
@@ -13,13 +17,16 @@ const CommunityKeywordList = () => {
   }
 
   return (
-    <ul>
-      {fetchedKeywordData?.map((keywordData) => (
-        <li key={keywordData.keywordId}>
-          {keywordData.keywordName} ({keywordData.memberCount})
-        </li>
-      ))}
-    </ul>
+    <>
+      <h3 className={cx('list-title')}>키워드 목록</h3>
+      <ul className={cx('list-container')}>
+        {fetchedKeywordData?.map((keywordData) => (
+          <li key={keywordData.keywordId} className={cx('list-item')}>
+            {keywordData.keywordName} ({keywordData.memberCount})
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
