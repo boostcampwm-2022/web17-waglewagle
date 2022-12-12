a5b3ff0e6cae200087c3fe9ef0cb3ba3471393bf
문성현 <shine7329@gmail.com>
2022-12-12 18:34:15 +0900

    [feat] thread 관련 query 에러 메세지 출력 기능

--- frontend/hooks/thread/useAddThreadMutation.tsx
+++ frontend/hooks/thread/useAddThreadMutation.tsx
@@ -1,13 +1,32 @@
+import type { ThreadData } from '#types/types';
 import { apis } from '@apis/index';
 import { useMutation } from '@tanstack/react-query';
+import type { AxiosError } from 'axios';
+import axios from 'axios';
 
-const useAddThreadMutation = (keywordId: string, content: string) => {
-  const { mutate } = useMutation({
-    mutationFn: () =>
-      apis.thread.addThread({
+interface UseAddThreadMutationProps {
+  keywordId: string;
+  content: string;
+}
+
+const useAddThreadMutation = ({
+  keywordId,
+  content,
+}: UseAddThreadMutationProps) => {
+  const { mutate } = useMutation<ThreadData, AxiosError>({
+    mutationFn: async () => {
+      const { data } = await apis.thread.addThread({
         keywordId,
         content,
-      }),
+      });
+      return data;
+    },
+    onError: (error) => {
+      const message = axios.isAxiosError(error)
+        ? error.response?.data.message
+        : '알 수 없는 에러가 발생했어요!🫢';
+      alert(message);
+    },
   });
 
   return { mutate };
