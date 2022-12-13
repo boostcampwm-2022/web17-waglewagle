87380c28303e5be3166e633104ece95ad3a7cb06
vangona <rlarhksrud14@gmail.com>
2022-12-13 19:29:13 +0900

    [refactor] useMyKeywordQuery 예외처리 및 리팩토링

--- frontend/hooks/keyword/useMyKeywordQuery.tsx
+++ frontend/hooks/keyword/useMyKeywordQuery.tsx
@@ -1,7 +1,8 @@
-import type { MyKeywordData } from '#types/types';
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
 import { apis } from '@apis/index';
 import { REACT_QUERY_KEY } from '@constants/constants';
-import { useQuery } from '@tanstack/react-query';
+import type { MyKeywordData } from '#types/types';
 
 const useMyKeywordQuery = (communityId: string) => {
   const { data } = useQuery<MyKeywordData[]>(
@@ -13,6 +14,12 @@ const useMyKeywordQuery = (communityId: string) => {
     {
       initialData: [],
       enabled: !!communityId,
+      onError: (error) => {
+        const message = axios.isAxiosError(error)
+          ? error.response?.data.message
+          : '내 키워드 목록을 읽어오던 중, 알 수 없는 에러가 발생했어요!';
+        alert(message);
+      },
     },
   );
 
