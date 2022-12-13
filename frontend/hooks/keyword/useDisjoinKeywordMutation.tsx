f75480826f8135ba28a32ca642ced75dd6a97b60
vangona <rlarhksrud14@gmail.com>
2022-12-13 19:13:11 +0900

    [fix] useDisjoinKeywordMutation 성공 메시지 및 예외처리

--- frontend/hooks/keyword/useDisjoinKeywordMutation.tsx
+++ frontend/hooks/keyword/useDisjoinKeywordMutation.tsx
@@ -3,19 +3,22 @@ import axios from 'axios';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { apis } from '@apis/index';
 import { REACT_QUERY_KEY } from '@constants/constants';
-import type { JoinKeywordData, MyKeywordData } from '#types/types';
+import type { AxiosError } from 'axios';
+import type { DisjoinKeywordData, MyKeywordData } from '#types/types';
 
 const useDisjoinKeywordMutation = () => {
   const router = useRouter();
   const communityId = router.query.id as string;
   const queryClient = useQueryClient();
 
-  const mutateDisjoinKeyword = async (disjoinKeywordData: JoinKeywordData) => {
+  const mutateDisjoinKeyword = async (
+    disjoinKeywordData: DisjoinKeywordData,
+  ) => {
     await apis.keyword.disjoinKeyword(disjoinKeywordData);
   };
 
   const updateMyKeywodList = (
-    disjoinKeywordData: JoinKeywordData,
+    disjoinKeywordData: DisjoinKeywordData,
     old?: MyKeywordData[],
   ): MyKeywordData[] => {
     if (!old) {
@@ -27,21 +30,28 @@ const useDisjoinKeywordMutation = () => {
     );
   };
 
-  const { mutate, isError, error } = useMutation({
+  const setNewKeywordList = (disjoinKeywordData: DisjoinKeywordData) => {
+    queryClient.setQueryData(
+      [REACT_QUERY_KEY.MY_KEYWORD_LIST, communityId],
+      (old: MyKeywordData[] | undefined) =>
+        updateMyKeywodList(disjoinKeywordData, old),
+    );
+  };
+
+  const { mutate, isError, error } = useMutation<
+    void,
+    AxiosError,
+    DisjoinKeywordData
+  >({
     mutationFn: mutateDisjoinKeyword,
     onSuccess: (_, disjoinKeywordData) => {
-      queryClient.setQueryData(
-        [REACT_QUERY_KEY.MY_KEYWORD_LIST, communityId],
-        (old: MyKeywordData[] | undefined) =>
-          updateMyKeywodList(disjoinKeywordData, old),
-      );
-
-      alert('관심 해제 했습니다.');
+      setNewKeywordList(disjoinKeywordData);
+      alert('관심 해제 되었습니다.');
     },
     onError: (error) => {
       const message = axios.isAxiosError(error)
         ? error.response?.data.message
-        : '키워드 추가 중, 알 수 없는 에러가 발생했어요!';
+        : '키워드 관심 해제 중, 알 수 없는 에러가 발생했어요!';
       alert(message);
     },
   });
