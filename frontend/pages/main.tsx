ee6ceaac9b6bfd0c180fb15d0759d0d85703917e
문성현 <shine7329@gmail.com>
2022-12-15 00:43:55 +0900

    [fix] 랜딩, 메인 페이지에서 커뮤니티 가입 요청하는 현상 해결

--- frontend/pages/main.tsx
+++ frontend/pages/main.tsx
@@ -1,6 +1,5 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import { apis } from '@apis/index';
 import { Loading } from '@components/common';
 import SeoHead from '@components/common/Head';
 import {
@@ -9,7 +8,6 @@ import {
   MainLayout,
   MainTitle,
 } from '@components/main';
-import { MVP_DEFAULT } from '@constants/constants';
 import useUserMe from '@hooks/useUserMe';
 import config from '../config';
 
@@ -18,16 +16,9 @@ const Main = () => {
   const userData = useUserMe();
 
   useEffect(() => {
-    // try / catch로 depth가 깊어져서 ealry return
     if (!userData) {
       router.push('/');
       return;
-    }
-
-    try {
-      apis.user.joinCommunity(MVP_DEFAULT.COMMUNITY_ID);
-    } catch (e) {
-      alert('알 수 없는 에러가 발생했습니다.');
     }
   }, [router, userData]);
 
