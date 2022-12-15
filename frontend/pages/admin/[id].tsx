c48f8b576b87b409c3f1855865ef057236f13429
김관경 <69471032+vangona@users.noreply.github.com>
2022-12-15 12:28:26 +0900

    Revert "[chore] frontend 프로젝트 폴더 삭제(be)"

---
+++ frontend/pages/admin/[id].tsx
@@ -0,0 +1,36 @@
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import {
+  AdminLayout,
+  AdminMain,
+  AdminSideBar,
+  KeywordControl,
+  UserControl,
+} from '@components/admin';
+import { ADMIN_PAGE_TAB } from '@constants/constants';
+
+const Admin = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  const [tab, setTab] = useState(0);
+  const [title, setTitle] = useState('부스트캠프 7기 커뮤니티');
+
+  const handleClickTab = (tabData: number) => {
+    setTab(tabData);
+  };
+
+  return (
+    <AdminLayout>
+      <AdminSideBar title={title} handleClickTab={handleClickTab} tab={tab} />
+      <AdminMain title={title} tab={tab}>
+        {tab === ADMIN_PAGE_TAB.USER_CONTROL ? (
+          <UserControl />
+        ) : (
+          <KeywordControl />
+        )}
+      </AdminMain>
+    </AdminLayout>
+  );
+};
+
+export default Admin;
