9a97389ea97ddf3c948871feb4c3bc18a944937c
vangona <rlarhksrud14@gmail.com>
2022-12-15 19:49:23 +0900

    [feat] 키워드 목록 클릭 시 스레드 입장 구현

--- frontend/components/community/CommunityKeywordList.tsx
+++ frontend/components/community/CommunityKeywordList.tsx
@@ -3,14 +3,28 @@ import classnames from 'classnames/bind';
 import { Loading } from '@components/common';
 import { useKeywordListQuery } from '@hooks/keyword';
 import styles from '@sass/components/community/CommunityKeywordList.module.scss';
+import { KeywordData, KeywordGroupData } from '#types/types';
 
 const cx = classnames.bind(styles);
 
-const CommunityKeywordList = () => {
+interface CommunityKeywordListProps {
+  handleChangeKeywordGroupData: (newKeywordGroupDate: KeywordGroupData) => void;
+}
+
+const CommunityKeywordList = ({
+  handleChangeKeywordGroupData,
+}: CommunityKeywordListProps) => {
   const router = useRouter();
   const communityId: string = router.query.id as string;
   const { data: fetchedKeywordData, isLoading } =
     useKeywordListQuery(communityId);
+
+  const onClickItem = (keywordData: KeywordData) => {
+    handleChangeKeywordGroupData({
+      keywordId: keywordData.keywordId,
+      keyword: keywordData.keywordName,
+    });
+  };
 
   if (isLoading) {
     return <Loading />;
@@ -21,7 +35,11 @@ const CommunityKeywordList = () => {
       <h3 className={cx('list-title')}>키워드 목록</h3>
       <ul className={cx('list-container')}>
         {fetchedKeywordData?.map((keywordData) => (
-          <li key={keywordData.keywordId} className={cx('list-item')}>
+          <li
+            key={keywordData.keywordId}
+            onClick={() => onClickItem(keywordData)}
+            className={cx('list-item')}
+          >
             {keywordData.keywordName} ({keywordData.memberCount})
           </li>
         ))}
