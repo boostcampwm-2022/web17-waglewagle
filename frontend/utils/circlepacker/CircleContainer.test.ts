73dcb6530adba715e21e135dd25b228eb332a53b
vangona <rlarhksrud14@gmail.com>
2022-12-15 18:47:56 +0900

    [test] CircleContainer 테스트 코드 수정
    Co-authored-by: 문성현 <shine7329@gmail.com>

--- frontend/utils/circlepacker/CircleContainer.test.ts
+++ frontend/utils/circlepacker/CircleContainer.test.ts
@@ -1,4 +1,3 @@
-import Circle from './Circle';
 import CircleContainer from './CircleContainer';
 
 type Coordinates = {
@@ -36,6 +35,17 @@ describe('CircleContainer', () => {
     expect(circleContainer.circles['1']).toMatchObject({ id: '1' });
   });
 
+  it('이미 있는 원 일 경우, 추가가 아닌 갱신', () => {
+    circleContainer.addCircle('1', 1);
+    const initialCircle = { ...circleContainer.circles['1'] };
+
+    circleContainer.addCircle('1', 2);
+    const updatedCircle = { ...circleContainer.circles['1'] };
+
+    expect(Object.keys(circleContainer.circles)).toHaveLength(1);
+    expect(updatedCircle.radius === initialCircle.radius).toBeFalsy();
+  });
+
   it('생성된 원은 중심 방향의 속도를 가진다.', () => {
     circleContainer.addCircle('1', 1);
     const initialCircle = { ...circleContainer.circles['1'] };
@@ -58,22 +68,45 @@ describe('CircleContainer', () => {
     );
   });
 
-  it('겹치는 원이 있다면 handleIntersection 메소드가 호출된다.', () => {
+  it('두 원이 겹친 경우 handleIntersection 메소드만 호출된다.', () => {
     const handleIntersection = jest.fn();
-    circleContainer.handleIntersection = handleIntersection;
-    circleContainer.circles = {
-      1: new Circle('1', 0, 0, 10, { x: 0, y: 0 }),
-      2: new Circle('2', 1, 1, 10, { x: 0, y: 0 }),
-    };
+    const handleCollision = jest.fn();
+    circleContainer['handleIntersection'] = handleIntersection;
+    circleContainer['handleCollision'] = handleCollision;
+    circleContainer.addCircle('1', 10, { x: 501, y: 501 });
+    circleContainer.addCircle('2', 10, { x: 500, y: 500 });
     circleContainer.update();
     expect(handleIntersection).toHaveBeenCalledTimes(1);
+    expect(handleCollision).toHaveBeenCalledTimes(0);
   });
 
-  it('충돌이 발생한 두 원은 거리가 멀어진다.', () => {
-    circleContainer.circles = {
-      1: new Circle('1', 500, 500, 10, { x: 0, y: 0 }),
-      2: new Circle('2', 501, 501, 10, { x: 0, y: 0 }),
-    };
+  it('두 원이 충돌한 경우 handleCollision 메소드만 호출된다.', () => {
+    const handleIntersection = jest.fn();
+    const handleCollision = jest.fn();
+    circleContainer['handleIntersection'] = handleIntersection;
+    circleContainer['handleCollision'] = handleCollision;
+    circleContainer.addCircle('1', 10, { x: 530, y: 500 });
+    circleContainer.addCircle('2', 10, { x: 500, y: 500 });
+    circleContainer.update();
+    expect(handleIntersection).toHaveBeenCalledTimes(0);
+    expect(handleCollision).toHaveBeenCalledTimes(1);
+  });
+
+  it('두 원이 거리가 먼 경우 handleIntersection, handleCollision 메소드 둘 다 호출되지 않는다.', () => {
+    const handleIntersection = jest.fn();
+    const handleCollision = jest.fn();
+    circleContainer['handleIntersection'] = handleIntersection;
+    circleContainer['handleCollision'] = handleCollision;
+    circleContainer.addCircle('1', 10, { x: 100, y: 100 });
+    circleContainer.addCircle('2', 10, { x: 300, y: 300 });
+    circleContainer.update();
+    expect(handleIntersection).toHaveBeenCalledTimes(0);
+    expect(handleCollision).toHaveBeenCalledTimes(0);
+  });
+
+  it('겹침이 발생한 두 원은 거리가 멀어진다.', () => {
+    circleContainer.addCircle('1', 10, { x: 501, y: 501 });
+    circleContainer.addCircle('2', 10, { x: 500, y: 500 });
     const initialDistance = calcDistance(
       circleContainer.circles['1'],
       circleContainer.circles['2'],
@@ -84,5 +117,51 @@ describe('CircleContainer', () => {
       circleContainer.circles['2'],
     );
     expect(updatedDistance).toBeGreaterThan(initialDistance);
+  });
+
+  it('충돌이 발생한 두 원은 거리가 멀어진다.', () => {
+    circleContainer.addCircle('1', 10, { x: 530, y: 500 });
+    circleContainer.addCircle('2', 10, { x: 500, y: 500 });
+    const initialDistance = calcDistance(
+      circleContainer.circles['1'],
+      circleContainer.circles['2'],
+    );
+    circleContainer.update();
+    const updatedDistance = calcDistance(
+      circleContainer.circles['1'],
+      circleContainer.circles['2'],
+    );
+    expect(updatedDistance).toBeGreaterThan(initialDistance);
+  });
+
+  it('겹침이 발생했을 때, 충돌이 발생한 경우보다 더 멀어진다.', () => {
+    circleContainer.addCircle('1', 10, { x: 330, y: 300 });
+    circleContainer.addCircle('2', 10, { x: 300, y: 300 });
+    const initialDistanceCollision = calcDistance(
+      circleContainer.circles['1'],
+      circleContainer.circles['2'],
+    );
+    circleContainer.update();
+    const updatedDistanceCollision = calcDistance(
+      circleContainer.circles['1'],
+      circleContainer.circles['2'],
+    );
+    const collisionDelta = updatedDistanceCollision - initialDistanceCollision;
+
+    circleContainer.addCircle('3', 10, { x: 501, y: 500 });
+    circleContainer.addCircle('4', 10, { x: 500, y: 500 });
+    const initialDistanceIntersection = calcDistance(
+      circleContainer.circles['3'],
+      circleContainer.circles['4'],
+    );
+    circleContainer.update();
+    const updatedDistanceIntersection = calcDistance(
+      circleContainer.circles['3'],
+      circleContainer.circles['4'],
+    );
+    const intersectionDelta =
+      updatedDistanceIntersection - initialDistanceIntersection;
+
+    expect(intersectionDelta).toBeGreaterThan(collisionDelta);
   });
 });
