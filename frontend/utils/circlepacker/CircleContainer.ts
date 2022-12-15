b732eee5b7d1b3ab77fefe338e903a09ff0bdf37
vangona <rlarhksrud14@gmail.com>
2022-12-15 15:45:54 +0900

    [refactor] circlecontainer 메서드 순서 변경
    Co-authored-by: 문성현 <shine7329@gmail.com>

--- frontend/utils/circlepacker/CircleContainer.ts
+++ frontend/utils/circlepacker/CircleContainer.ts
@@ -19,24 +19,6 @@ class CircleContainer {
     private gravityCoefficient: number = 98,
   ) {}
 
-  calcInitVector(x: number, y: number) {
-    const centralX = this.width / 2;
-    const centralY = this.height / 2;
-    const vector = {
-      x: 0,
-      y: 0,
-    };
-
-    // 초기 중력
-    vector.x = centralX - x;
-    vector.y = centralY - y;
-
-    vector.x /= 20;
-    vector.y /= 20;
-
-    return vector;
-  }
-
   getRandPosByMass(mass: number) {
     const centralX = this.width / 2;
     const centralY = this.height / 2;
@@ -58,61 +40,22 @@ class CircleContainer {
     };
   }
 
-  addCircle(circleId: string, radius: number) {
-    if (circleId in this.circles) {
-      const updatedCircle = this.circles[circleId];
-      if (!updatedCircle) {
-        throw new Error('없는 원입니다.');
-      }
-      updatedCircle.radius = radius;
-      return updatedCircle;
-    }
+  calcInitVector(x: number, y: number) {
+    const centralX = this.width / 2;
+    const centralY = this.height / 2;
+    const vector = {
+      x: 0,
+      y: 0,
+    };
 
-    const { x, y } = this.getRandPosByMass(radius);
-    const newCircle = new Circle(
-      circleId,
-      x,
-      y,
-      (radius * this.width) / 2000 + 10,
-      this.calcInitVector(x, y),
-    );
-    this.circles[circleId] = newCircle;
+    // 초기 중력
+    vector.x = centralX - x;
+    vector.y = centralY - y;
 
-    return newCircle;
-  }
+    vector.x /= 20;
+    vector.y /= 20;
 
-  // 화면 갱신
-  update() {
-    const idArray = Object.keys(this.circles);
-    for (let i = 0; i < idArray.length; i++) {
-      const circleA = this.circles[idArray[i]];
-      for (let j = i + 1; j < idArray.length; j++) {
-        const circleB = this.circles[idArray[j]];
-        const intersectionType = this.checkIntersection(circleA, circleB);
-        if (intersectionType !== INTERSECTION_TYPES.NO_COLLISION) {
-          this.handleIntersection(circleA, circleB, intersectionType);
-        }
-      }
-      this.applyGravity(circleA);
-      this.handleWallCollision(circleA);
-      circleA.move();
-    }
-  }
-
-  handleWallCollision(circle: Circle) {
-    if (
-      circle.x - circle.radius <= 0 ||
-      circle.x + circle.radius >= this.width
-    ) {
-      circle.velocity.x *= -1;
-    }
-
-    if (
-      circle.y - circle.radius <= 0 ||
-      circle.y + circle.radius >= this.height
-    ) {
-      circle.velocity.y *= -1;
-    }
+    return vector;
   }
 
   // 충돌 확인
@@ -131,37 +74,17 @@ class CircleContainer {
     return INTERSECTION_TYPES.NO_COLLISION;
   }
 
-  // 스칼라를 계산해서 Vector를 구혀준다.
-  calculateIntersectionScala(
-    speedA: number,
-    speedB: number,
-    massA: number,
-    massB: number,
-    repulsiveForce: number,
+  // 겹침 발생시 속도를 변화시킴
+  handleIntersection(
+    circleA: Circle,
+    circleB: Circle,
     intersectionType: INTERSECTION_TYPES,
   ) {
-    const baseForce =
-      speedA + (2 * massB * (speedB - speedA)) / (massA + massB);
+    const { afterCircleAVelocity, afterCircleBVelocity } =
+      this.calculateIntersectionVector(circleA, circleB, intersectionType);
 
-    switch (intersectionType) {
-      case INTERSECTION_TYPES.INTERSECTION:
-        return (
-          (baseForce + repulsiveForce * INTERSECTION_REPULSIVE_COEFFICIENT) *
-          COLLISION_COEFFICIENT
-        );
-
-      case INTERSECTION_TYPES.COLLISION:
-        return (
-          (baseForce + repulsiveForce * REPULSIVE_COEFFICIENT) *
-          COLLISION_COEFFICIENT
-        );
-
-      default:
-        return (
-          (baseForce + repulsiveForce * REPULSIVE_COEFFICIENT) *
-          COLLISION_COEFFICIENT
-        );
-    }
+    circleA.velocity = afterCircleAVelocity;
+    circleB.velocity = afterCircleBVelocity;
   }
 
   calculateIntersectionVector(
@@ -211,17 +134,53 @@ class CircleContainer {
     return { afterCircleAVelocity, afterCircleBVelocity };
   }
 
-  // // 겹침 발생시 속도를 변화시킴
-  handleIntersection(
-    circleA: Circle,
-    circleB: Circle,
+  // 스칼라를 계산해서 Vector를 구혀준다.
+  calculateIntersectionScala(
+    speedA: number,
+    speedB: number,
+    massA: number,
+    massB: number,
+    repulsiveForce: number,
     intersectionType: INTERSECTION_TYPES,
   ) {
-    const { afterCircleAVelocity, afterCircleBVelocity } =
-      this.calculateIntersectionVector(circleA, circleB, intersectionType);
+    const baseForce =
+      speedA + (2 * massB * (speedB - speedA)) / (massA + massB);
 
-    circleA.velocity = afterCircleAVelocity;
-    circleB.velocity = afterCircleBVelocity;
+    switch (intersectionType) {
+      case INTERSECTION_TYPES.INTERSECTION:
+        return (
+          (baseForce + repulsiveForce * INTERSECTION_REPULSIVE_COEFFICIENT) *
+          COLLISION_COEFFICIENT
+        );
+
+      case INTERSECTION_TYPES.COLLISION:
+        return (
+          (baseForce + repulsiveForce * REPULSIVE_COEFFICIENT) *
+          COLLISION_COEFFICIENT
+        );
+
+      default:
+        return (
+          (baseForce + repulsiveForce * REPULSIVE_COEFFICIENT) *
+          COLLISION_COEFFICIENT
+        );
+    }
+  }
+
+  handleWallCollision(circle: Circle) {
+    if (
+      circle.x - circle.radius <= 0 ||
+      circle.x + circle.radius >= this.width
+    ) {
+      circle.velocity.x *= -1;
+    }
+
+    if (
+      circle.y - circle.radius <= 0 ||
+      circle.y + circle.radius >= this.height
+    ) {
+      circle.velocity.y *= -1;
+    }
   }
 
   applyGravity(circle: Circle) {
@@ -247,6 +206,47 @@ class CircleContainer {
     this.width = newWidth;
     this.height = newHeight;
     this.gravityCoefficient = 98;
+  }
+
+  addCircle(circleId: string, radius: number) {
+    if (circleId in this.circles) {
+      const updatedCircle = this.circles[circleId];
+      if (!updatedCircle) {
+        throw new Error('없는 원입니다.');
+      }
+      updatedCircle.radius = radius;
+      return updatedCircle;
+    }
+
+    const { x, y } = this.getRandPosByMass(radius);
+    const newCircle = new Circle(
+      circleId,
+      x,
+      y,
+      (radius * this.width) / 2000 + 10,
+      this.calcInitVector(x, y),
+    );
+    this.circles[circleId] = newCircle;
+
+    return newCircle;
+  }
+
+  // 화면 갱신
+  update() {
+    const idArray = Object.keys(this.circles);
+    for (let i = 0; i < idArray.length; i++) {
+      const circleA = this.circles[idArray[i]];
+      for (let j = i + 1; j < idArray.length; j++) {
+        const circleB = this.circles[idArray[j]];
+        const intersectionType = this.checkIntersection(circleA, circleB);
+        if (intersectionType !== INTERSECTION_TYPES.NO_COLLISION) {
+          this.handleIntersection(circleA, circleB, intersectionType);
+        }
+      }
+      this.applyGravity(circleA);
+      this.handleWallCollision(circleA);
+      circleA.move();
+    }
   }
 }
 
