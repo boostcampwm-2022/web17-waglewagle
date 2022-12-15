35e288fcafa6490dc32ed895f02b0124282b8d89
김관경 <69471032+vangona@users.noreply.github.com>
2022-12-15 12:26:38 +0900

    Revert "[Week06/Day04] Release to Main"

--- rest/src/main/java/com/waglewagle/rest/user/exception/UnauthorizedException.java
+++
@@ -1,9 +0,0 @@
-package com.waglewagle.rest.user.exception;
-
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException() {
-        super(ExceptionMessage.UNAUTHORIZED.getMessage());
-    }
-
-}
-
