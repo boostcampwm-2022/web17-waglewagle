07b6222df3f2b5de91b963b241f8f3468921fe32
문성현 <shine7329@gmail.com>
2022-12-18 17:07:39 +0900

    [chore] 관리자 페이지 스타일 수정

--- frontend/components/admin/AdminMain.tsx
+++ frontend/components/admin/AdminMain.tsx
@@ -2,20 +2,20 @@ import { ReactNode } from 'react';
 import classnames from 'classnames/bind';
 import { ADMIN_PAGE_TAB } from '@constants/constants';
 import styles from '@sass/components/admin/AdminMain.module.scss';
+
 const cx = classnames.bind(styles);
 
 interface AdminMainProps {
-  title: string;
   tab: number;
   children: ReactNode;
 }
 
-const AdminMain = ({ title, tab, children }: AdminMainProps) => {
+const AdminMain = ({ tab, children }: AdminMainProps) => {
   return (
     <main className={cx('main')}>
       <header>
         <h3>
-          {title} {tab === ADMIN_PAGE_TAB.KEYWORD_CONTROL ? '키워드' : '유저'}{' '}
+          커뮤니티 {tab === ADMIN_PAGE_TAB.KEYWORD_CONTROL ? '키워드' : '유저'}{' '}
           관리
         </h3>
       </header>
